3c1d6594635c1e9196a16111353d124da9f5a795
Nate Schultz <nate.schultz@outlook.com>
2016-02-27 04:00:20 -0500

    add friend dialog, cleaned up java, deleted old tests

--- SuchWortDo/app/src/main/java/com/suchwortdo/suchwortdo/MainActivity.java
+++ SuchWortDo/app/src/main/java/com/suchwortdo/suchwortdo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.suchwortdo.suchwortdo;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 
 import android.support.v4.app.Fragment;
@@ -26,6 +29,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static boolean init = true;
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -54,101 +59,30 @@ public class MainActivity extends AppCompatActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.setCurrentItem(2);
     }
 
+    public static Dialog onCreateDialog(Bundle savedInstanceState, View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+        // Get the layout inflater
+        LayoutInflater inflater = LayoutInflater.from(v.getContext());
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            String[] page_title = {"Settings","Friend List","Friends","Local","Global"};
-            final View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            int section_num = getArguments().getInt(ARG_SECTION_NUMBER)-1;
-            textView.setText(page_title[section_num]);
-
-            // hide/show contents for seperate pages
-            final EditText msg_box = (EditText) rootView.findViewById(R.id.msg_box);
-            Button add_friend_btn = (Button) rootView.findViewById(R.id.add_friend_btn);
-            if(section_num == 0) {
-                // settings page
-                msg_box.setVisibility(rootView.GONE);
-                add_friend_btn.setVisibility(rootView.GONE);
-                hideKeyboard(getActivity());
-            } else if(section_num == 1) {
-                // friend list page
-                msg_box.setVisibility(rootView.GONE);
-                add_friend_btn.setVisibility(rootView.VISIBLE);
-                hideKeyboard(getActivity());
-
-                // friends
-                String[] friends = {"bob","george","sally","bob","george","sally","bob","george","sally","bob","george","sally"};
-                ArrayAdapter<String> friends_Adapter =
-                        new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, friends);
-                ListView friends_listView = (ListView) rootView.findViewById(R.id.friend_list);
-                friends_listView.setAdapter(friends_Adapter);
-
-                // keys
-                String[] keys = {"5FWAS","234SD","WWWWW","5FWAS","234SD","WWWWW","5FWAS","234SD","WWWWW","5FWAS","234SD","WWWWW"};
-                ArrayAdapter<String> keys_Adapter =
-                        new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, keys);
-                ListView keys_listView = (ListView) rootView.findViewById(R.id.key_list);
-                keys_listView.setAdapter(keys_Adapter);
-            } else {
-                msg_box.setVisibility(rootView.VISIBLE);
-                add_friend_btn.setVisibility(rootView.GONE);
-
-                msg_box.setOnKeyListener(new View.OnKeyListener() {
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.dialog_add_friend, null))
+                // Add action buttons
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
                     @Override
-                    public boolean onKey(View v, int keyCode, KeyEvent event) {
-                        if( keyCode == KeyEvent.KEYCODE_ENTER ) {
-                            if( event.getAction() == KeyEvent.ACTION_UP ) {
-                                if(msg_box.getText().length() == 0) {
-                                    shakeAnimation(rootView);
-                                } else {
-                                    sendMsgAnimation(rootView);
-                                }
-                            }
-                            return true;
-                        }
-                        return false;
+                    public void onClick(DialogInterface dialog, int id) {
+                        // sign in the user ...
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
                     }
                 });
-            }
-
-            return rootView;
-        }
+        return builder.create();
     }
 
     public static void shakeAnimation(View v) {
@@ -187,51 +121,6 @@ public class MainActivity extends AppCompatActivity {
         v.findViewById(R.id.msg_box).startAnimation(move_up);
         v.findViewById(R.id.anim_msg_box).startAnimation(replace);
         v.findViewById(R.id.anim_msg_box).setVisibility(View.INVISIBLE);
-    }
-
-    public static boolean send_msg() {
-        return true;
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 5;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "Settings";
-                case 1:
-                    return "Friend List";
-                case 2:
-                    return "Friends";
-                case 3:
-                    return "Local";
-                case 4:
-                    return "Global";
-            }
-            return null;
-        }
     }
 
     public static void hideKeyboard(Activity activity) {
