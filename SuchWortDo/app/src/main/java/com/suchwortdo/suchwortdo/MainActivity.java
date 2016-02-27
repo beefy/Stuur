5f9f3852c294fab4167a28c8d34ef0528750bde0
Nate Schultz <nate.schultz@outlook.com>
2016-02-27 01:24:49 -0500

    added animations, bug fixes

--- SuchWortDo/app/src/main/java/com/suchwortdo/suchwortdo/MainActivity.java
+++ SuchWortDo/app/src/main/java/com/suchwortdo/suchwortdo/MainActivity.java
@@ -1,9 +1,7 @@
 package com.suchwortdo.suchwortdo;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -11,20 +9,21 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.HashMap;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -94,22 +93,24 @@ public class MainActivity extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             String[] page_title = {"Settings","Friend List","Friends","Local","Global"};
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            final View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
             int section_num = getArguments().getInt(ARG_SECTION_NUMBER)-1;
             textView.setText(page_title[section_num]);
 
-            // hide message box for settings and friend list screens
-            EditText msg_box = (EditText) rootView.findViewById(R.id.msg_box);
+            // hide/show contents for seperate pages
+            final EditText msg_box = (EditText) rootView.findViewById(R.id.msg_box);
             Button add_friend_btn = (Button) rootView.findViewById(R.id.add_friend_btn);
             if(section_num == 0) {
                 // settings page
                 msg_box.setVisibility(rootView.GONE);
                 add_friend_btn.setVisibility(rootView.GONE);
+                hideKeyboard(getActivity());
             } else if(section_num == 1) {
                 // friend list page
                 msg_box.setVisibility(rootView.GONE);
                 add_friend_btn.setVisibility(rootView.VISIBLE);
+                hideKeyboard(getActivity());
 
                 // friends
                 String[] friends = {"bob","george","sally","bob","george","sally","bob","george","sally","bob","george","sally"};
@@ -125,12 +126,71 @@ public class MainActivity extends AppCompatActivity {
                 ListView keys_listView = (ListView) rootView.findViewById(R.id.key_list);
                 keys_listView.setAdapter(keys_Adapter);
             } else {
-                    msg_box.setVisibility(rootView.VISIBLE);
+                msg_box.setVisibility(rootView.VISIBLE);
                 add_friend_btn.setVisibility(rootView.GONE);
+
+                msg_box.setOnKeyListener(new View.OnKeyListener() {
+                    @Override
+                    public boolean onKey(View v, int keyCode, KeyEvent event) {
+                        if( keyCode == KeyEvent.KEYCODE_ENTER ) {
+                            if( event.getAction() == KeyEvent.ACTION_UP ) {
+                                if(msg_box.getText().length() == 0) {
+                                    shakeAnimation(rootView);
+                                } else {
+                                    sendMsgAnimation(rootView);
+                                }
+                            }
+                            return true;
+                        }
+                        return false;
+                    }
+                });
             }
 
             return rootView;
         }
+    }
+
+    public static void shakeAnimation(View v) {
+        Animation shake = AnimationUtils.loadAnimation(v.getContext(), R.anim.shake);
+        v.findViewById(R.id.msg_box).startAnimation(shake);
+    }
+
+    public static void sendMsgAnimation(View v) {
+        final View finalv = v;
+        Animation move_up = AnimationUtils.loadAnimation(v.getContext(), R.anim.move_up);
+        Animation replace = AnimationUtils.loadAnimation(v.getContext(), R.anim.replace);
+
+        replace.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation arg0) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation arg0) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+                EditText editText = (EditText) finalv.findViewById(R.id.msg_box);
+                editText.setText("", TextView.BufferType.EDITABLE);
+
+                CharSequence text = "Message Sent";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(finalv.getContext(), text, duration);
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, -200);
+                toast.show();
+            }
+        });
+
+        v.findViewById(R.id.anim_msg_box).setVisibility(View.VISIBLE);
+        v.findViewById(R.id.msg_box).startAnimation(move_up);
+        v.findViewById(R.id.anim_msg_box).startAnimation(replace);
+        v.findViewById(R.id.anim_msg_box).setVisibility(View.INVISIBLE);
+    }
+
+    public static boolean send_msg() {
+        return true;
     }
 
     /**
@@ -172,5 +232,16 @@ public class MainActivity extends AppCompatActivity {
             }
             return null;
         }
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
